4ef2d1bb6c0e5718a2b3eac593940ba47b760769
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-03-06 00:45:36 -0700

    Exit when file type not supported

--- node/index.js
+++ node/index.js
@@ -4,6 +4,12 @@ const path = require("path")
 const main = () => {
     const fileName = process.argv[2];
     const fileExt = path.extname(fileName);
+
+    if(fileExt.toLowerCase() !== ".png"){
+        console.log("Only png is supported currently.")
+        process.exit(1);
+    }
+
     const fileContents = fs.readFileSync(fileName);
     const data = Buffer.from(fileContents).toString("base64");
     const output = `module.exports="data:image/png;base64,${data}";`
